23d0b1c5188c6851d41ea53c9e43ad127389efef
MohammedMaher177 <anahammo777@gmail.com>
2023-11-11 17:10:09 +0200

    test'

---
+++ src/components/Calculator/Calculator.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="calc">
+    <div class="header">0</div>
+    <button type="number">1</button>
+    <button type="number">2</button>
+  </div>
+</template>
+
+<style>
+.calc {
+  width: 500px;
+}
+
+.header {
+  background: #ccc;
+  padding: 1rem 3rem;
+}
+
+.p-5 {
+}
+</style>
